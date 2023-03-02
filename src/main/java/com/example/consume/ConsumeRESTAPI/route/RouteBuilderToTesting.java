e9acaa588df1d02388e8a4977e24816a4d0cf8d1
Amit Pathak <43213063+amitp11@users.noreply.github.com>
2023-03-02 09:04:23 +0530

    Add files via upload

---
+++ src/main/java/com/example/consume/ConsumeRESTAPI/route/RouteBuilderToTesting.java
@@ -0,0 +1,28 @@
+package com.example.consume.ConsumeRESTAPI.route;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.example.consume.ConsumeRESTAPI.processor.MyProcessor;
+
+@Component
+public class RouteBuilderToTesting extends RouteBuilder {
+	
+	@Value(value = "${testsourceep}")
+	private String sourceEndPoint;
+	
+	@Override
+	public void configure() throws Exception {
+		
+		from("timer:rest-client?period=10s")
+		.log("REST API Calling .....")
+		.setHeader(Exchange.HTTP_METHOD,simple("GET"))
+		.to(sourceEndPoint)
+		.log("Incoming Body is ${body}");
+		//.process(new MyProcessor());
+
+	}
+
+}
